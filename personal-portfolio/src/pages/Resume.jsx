ebf7c7db34cd65c12eebb2bf0689e530bbf320f5
Rishan Biju <rishanbiju@gmail.com>
2024-05-07 15:39:02 -0400

    Fixed All Issues

--- personal-portfolio/src/pages/Resume.jsx
+++ personal-portfolio/src/pages/Resume.jsx
@@ -8,6 +8,7 @@ const Resume = () => {
     <div className='my-40 flex flex-col justify-center items-center text-center gap-20'>
       <h1 style={{fontFamily: 'Consolas', color: 'white', fontSize: 30}}><u>Resume</u></h1>
       <iframe src={PersonalResume} title='Rishan Biju Resume' height={300} width={300} loading='lazy'></iframe>
+      <a href="https://drive.google.com/file/d/1yQHRvIQ8HAVLqoeUVjhlQfrWhLJfrBKg/view?usp=sharing" style={{color:'white', fontFamily:'Consolas', fontSize:20}}>Click Me to Access Full Resume</a>
     </div>
 
     <Social />

ef2f06c4d9cc92971aaebc28105fea622bf62f78
Rishan Biju <rishanbiju@gmail.com>
2024-06-17 12:11:48 -0400

    Tweaked file

--- personal-portfolio/src/pages/Resume.jsx
+++ personal-portfolio/src/pages/Resume.jsx
@@ -9,7 +9,7 @@ const Resume = () => {
     <div className='my-40 flex flex-col justify-center items-center text-center gap-20'>
       <h1 style={{fontFamily: 'Poppins', color: '#1e293b', fontSize: 30, fontWeight: 'bold'}}><u>Resume</u></h1>
       <iframe src={PersonalResume} title='Rishan Biju Resume' height={300} width={300} loading='lazy'></iframe>
-      <a href="https://drive.google.com/file/d/1EuZvd184qIOX0L4-baE1kpJ_n9k5Wu-v/view?usp=sharing" style={{color:'#1e293b', fontFamily:'Poppins', fontSize:20, fontWeight: 'bold'}}>Click Me to Access Full Resume</a>
+      <a href="https://drive.google.com/file/d/10hOIHiLYZatl_XtNyqHVr3AfxhFT6Sju/view?usp=sharing" style={{color:'#1e293b', fontFamily:'Poppins', fontSize:20, fontWeight: 'bold'}}>Click Me to Access Full Resume</a>
     </div>
 
     <Social />
